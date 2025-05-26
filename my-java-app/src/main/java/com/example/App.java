cda41756ce6c23360ebdb523d59346fa5c800b0c
gopalraj321 <162572006+gopalraj321@users.noreply.github.com>
2025-05-26 15:24:10 +0530

    Update App.java

--- my-java-app/src/main/java/com/example/App.java
+++ my-java-app/src/main/java/com/example/App.java
@@ -29,6 +29,6 @@ public class App {
 
     // 🔥 Required for your test case
     public static String greet() {
-        return "Hello from greet() method!";
+        return "Hello! MR GOpalraj wellcome to our World";
     }
 }
