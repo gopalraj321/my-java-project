a5e63e4b333d311babcd44f14ca538330a198032
gopalraj321 <gopalraj321@gmail.com>
2025-06-11 22:11:54 +0530

    Initial commit

---
+++ java-cicd-demo/src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World from Java CI/CD Project!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
